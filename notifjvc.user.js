65edae1898d821ef28d27634dea5327015de0761
JulesVautier <julesvautier@hotmail.fr>
2022-10-23 18:38:16 +0200

    Fix Notif creation and add date in post

--- notifjvc.user.js
+++ notifjvc.user.js
@@ -23,6 +23,19 @@ function decodeHTMLEntities(text) {
     return html.text();
 }
 
+function searchAllOccurences(page, message) {
+    var startingIndices = [];
+
+    var indexOccurence = page.indexOf(message, 0);
+
+    while(indexOccurence >= 0) {
+        startingIndices.push(indexOccurence);
+
+        indexOccurence = page.indexOf(message, indexOccurence + 1);
+    }
+    return startingIndices
+}
+
 
 class Notification {
     constructor(topicUrl, message, messageUrl) {
@@ -32,29 +45,51 @@ class Notification {
     }
 
     exists() {
-        return getNotifs().find(element => {
-            if (element.topicUrl == this.topicUrl && element.message == this.message) {
+        var exist = getNotifs().find(element => {
+            if (element.topicUrl == this.topicUrl && element.message == this.message && element.position == this.position) {
                 return true
             } else {
                 return false
             }
         })
+        if (exist == undefined) {
+            return false
+        } else {
+            return true
+        }
     }
 }
 
+
 class Post {
-    constructor(topicUrl, text) {
+    constructor(topicUrl, text, lastNotifDate) {
         this.topicUrl = topicUrl.split("#")[0];
         this.messages = text;
         this.pagesChecked = getPageNumber(topicUrl)
+        this.lastNotifDate = lastNotifDate
     }
 
     addMessage(text){
         this.messages.push(text)
     }
 
+    incrementPage(url) {
+        var tab = url.split('-')
+        var page = Number(tab[3])
+        tab[3] = page + 1
+        return tab.join('-')
+    }
+
     checkQuotedMessages() {
-        var url = this.topicUrl
+        if (this.checkQuotedMessagesFromPage(this.topicUrl) == true) {
+            var newUrl = this.incrementPage(this.topicUrl)
+//            if (this.checkQuotedMessagesFromPage(this.topicUrl) == true) {
+//                this.topicUrl = newUrl
+//            }
+        }
+    }
+
+    checkQuotedMessagesFromPage(url) {
         var req = new XMLHttpRequest();
 
         req.open('GET', url, false);
@@ -64,15 +99,18 @@ class Post {
             this.messages.forEach(message => {
                 var response = decodeHTMLEntities(req.response)
                 let position = response.search(message)
-                if(position != -1) {
-                    var notif = new Notification(this.topicUrl, message, "")
-                    if (!notif.exists()) {
-                        console.log('new_notif')
-                        addNotif(notif)
+                searchAllOccurences(response, message).forEach(position => {
+                    if (position != -1 && response[position + message.length + 0] != '\n') {
+                        var notif = new Notification(this.topicUrl, message, position)
+                        if (!notif.exists()) {
+                            addNotif(notif)
+                        }
                     }
-                }
+                })
             })
+            return true
         }
+        return false
     }
 }
 
@@ -90,8 +128,7 @@ function getPosts() {
     var posts_tmp = $.parseJSON(cook)
     var posts_list = []
     posts_tmp.forEach(post_tmp => {
-        console.log('get_posts', post_tmp)
-        posts_list.push(new Post(post_tmp.topicUrl, post_tmp.messages))
+        posts_list.push(new Post(post_tmp.topicUrl, post_tmp.messages, post_tmp.lastNotifDate))
     })
     posts = posts_list
     return posts
@@ -99,7 +136,14 @@ function getPosts() {
 
 function addPost(post) {
     posts = getPosts()
-    posts.push(post)
+    var existingPost = posts.find(element => {
+        return String(element.topicUrl) == String(post.topicUrl)
+    })
+    if (existingPost == undefined) {
+        posts.push(post)
+    } else {
+        existingPost.messages.concat(post.messages)
+    }
     $.cookie(COOKIE_POSTS, JSON.stringify(posts));
     return posts
 }
@@ -114,7 +158,6 @@ function getNotifs() {
     var notifs_tmp = $.parseJSON(cook)
     var notifs_list = []
     notifs_tmp.forEach(notif_tmp => {
-        console.log(notif_tmp)
         notifs_list.push(new Notification(notif_tmp.topicUrl, notif_tmp.message, notif_tmp.messageUrl))
     })
     notifs = notifs_list
@@ -136,7 +179,7 @@ function resetCookies() {
 
 
 function savePost(topicUrl, text) {
-    var post = new Post(topicUrl, [text])
+    var post = new Post(topicUrl, [text], new Date().getTime())
     addPost(post)
 }
 
@@ -158,11 +201,10 @@ function readPost() {
     } else {
         savePost(topicUrl, text)
     }
-    checkQuotes()
 }
 
 function postOuCancer() {
-    alert("post ou cancer\n Le script de notif a besoin de la prévisualition pour fonctionner, tapez moi vie ahi")
+    alert("post ou cancer\n Le script de notif a besoin de la prévisualition pour fonctionner, tapez moins vite ahi(ent)")
     return true
 }
 
@@ -179,8 +221,10 @@ function addDebug() {
     $(".jv-editor-toolbar").append("<div id='DebugNotifJVC' class='btn-group'></div>")
     $("#DebugNotifJVC").append("<button id='RandomIssouReload' data-toggle='tooltip' title='DebugNotifJVC' class='btn btn-jv-editor-toolbar' type='button' value='RandomIssou' style='color: #FF0000; padding-top: 10px'>YAYAYAY</button>")
     $("#DebugNotifJVC").append("<button id='Ent' data-toggle='tooltip' title='DebugNotifJVC' class='btn btn-jv-editor-toolbar' type='button' value='RandomIssou' style='color: #FF0000; padding-top: 10px'>COKIES</button>")
+    $("#DebugNotifJVC").append("<button id='Notif' data-toggle='tooltip' title='DebugNotifJVC' class='btn btn-jv-editor-toolbar' type='button' value='RandomIssou' style='color: #FF0000; padding-top: 10px'>Notif</button>")
     document.getElementById("RandomIssouReload").onclick = readPost
     document.getElementById("Ent").onclick = resetCookies
+    document.getElementById("Notif").onclick = checkQuotes
 }
 
 
@@ -192,5 +236,4 @@ function checkQuotes() {
     var last10Posts = posts.slice(-10);
     last10Posts.forEach(post => {post.checkQuotedMessages()})
 }
-checkQuotes()
 //setInterval(checkQuotes, 5000)
